a3df40f35679d3dfd3e7847e4cf76f7ca29c0f69
robshee81 <121311059+robshee81@users.noreply.github.com>
2024-05-21 20:03:08 +1000

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@ VERSION=$(grep_prop version "${TMPDIR}/module.prop" | tr -d '\n')
 ui_print "- Android API level ${API}"
 ui_print "- Magisk version ${MAGISK_VER}"
 ui_print "- Module version ${VERSION}"
-if [ $API -lt 26 ]; then
+if [ $API -lt 27 ]; then
 # version < 8, no need to install
 	abort "! No restriction in current Android version!"
 fi
